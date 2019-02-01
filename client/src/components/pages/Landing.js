1573269d9b3716af940d0f042287fb9f90ffbaad
Brandon <BmcPrimary@yahoo.com>
2019-02-01 18:22:22 -0500

    Added search button on Landing page

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -7,11 +7,17 @@ const Landing = () => {
       <div className="landing">
         <div className="landing--center">
           <h1 className="landing__title">Search for Recipes</h1>
-          <input
-            className="landing__search"
-            type="textbox"
-            placeholder="Search Recipes"
-          />
+          <div className="landing__field">
+            <input
+              className="landing__search"
+              type="textbox"
+              placeholder="Search Recipes"
+              autoFocus
+            />
+            <button type="submit">
+              <i class="fas fa-arrow-right" />
+            </button>
+          </div>
         </div>
       </div>
     </div>
