0b1602b41a016f2455e70dedae57ae0fb2874d38
Brandon <BmcPrimary@yahoo.com>
2019-03-07 13:48:33 -0500

    Recipe results show on page once searched

--- client/src/components/pages/Recipes.js
+++ client/src/components/pages/Recipes.js
@@ -5,19 +5,14 @@ class Recipes extends Component {
   constructor() {
     super();
     this.state = {
-      recipes: {},
+      recipes: [],
     };
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.recipes) {
-      nextProps.recipes.forEach((element, index) => {
-        this.setState(prevState => ({
-          recipes: {
-            ...prevState.recipes,
-            [index]: element,
-          },
-        }));
+      this.setState({
+        recipes: nextProps.recipes,
       });
     }
   }
@@ -26,7 +21,23 @@ class Recipes extends Component {
     return (
       <div className="recipe">
         <div className="recipe__image" />
-        <div className="recipe__container">Test</div>
+        <div className="recipe__container">
+          <ul>
+            {this.state.recipes.map(recipe => (
+              <li key={recipe.recipe_id}>
+                <a className="results__link" href={`#${recipe.recipe_id}`}>
+                  <figure className="results__fig">
+                    <img src={recipe.image_url} alt={recipe.title} />
+                  </figure>
+                  <div className="results__data">
+                    <h4 className="results__name">${recipe.title}</h4>
+                    <p className="results__author">${recipe.publisher}</p>
+                  </div>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
