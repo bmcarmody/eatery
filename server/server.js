078d1698f368cce2efc8b947e4dddf7e066872ad
Brandon <BmcPrimary@yahoo.com>
2019-04-20 11:03:19 -0400

    Testing

--- server/server.js
+++ server/server.js
@@ -40,16 +40,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, '../', 'client', 'build', 'index.html')
-    );
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
 const port = process.env.PORT || 5000;
 
 const server = app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 // Exporting server for testing purposes
